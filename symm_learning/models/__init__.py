6b93531d34ae6ab3520939e0af0ae16207191188
Daniel Ordonez <daniels.ordonez@gmail.com>
2026-04-30 16:53:34 +0200

    v0.7.0-refactor: Move Transformer modules to nn.

--- symm_learning/models/__init__.py
+++ symm_learning/models/__init__.py
@@ -24,7 +24,6 @@ from .diffusion.econd_transformer_regressor import eCondTransformerRegressor
 from .emlp import MLP, eMLP, iMLP
 from .time_cnn.cnn_encoder import TimeCNNEncoder
 from .time_cnn.ecnn_encoder import eTimeCNNEncoder
-from .transformer.etransformer import eTransformerDecoderLayer, eTransformerEncoderLayer
 
 __all__ = [
     "eMLP",
@@ -32,8 +31,6 @@ __all__ = [
     "MLP",
     "eTimeCNNEncoder",
     "TimeCNNEncoder",
-    "eTransformerEncoderLayer",
-    "eTransformerDecoderLayer",
     "eCondTransformerRegressor",
     "CondTransformerRegressor",
     "GenCondRegressor",
